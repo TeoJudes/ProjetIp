378f84e05196593840418be2f8a56e358b675e5c
Téo Judes <judesteo@laposte.net>
2017-01-14 16:15:52 +0100

    commentaire

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace ProjetIP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbIP1 = new System.Windows.Forms.TextBox();
+            this.tbIP2 = new System.Windows.Forms.TextBox();
+            this.tbIP3 = new System.Windows.Forms.TextBox();
+            this.tbIP4 = new System.Windows.Forms.TextBox();
+            this.tbCIDR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btDetailIP = new System.Windows.Forms.Button();
+            this.lbReseau = new System.Windows.Forms.Label();
+            this.lbDiffusion = new System.Windows.Forms.Label();
+            this.lbPlage = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbMasque = new System.Windows.Forms.Label();
+            this.lbHote = new System.Windows.Forms.Label();
+            this.lbErreur = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbIP1
+            // 
+            this.tbIP1.Location = new System.Drawing.Point(41, 22);
+            this.tbIP1.MaxLength = 3;
+            this.tbIP1.Name = "tbIP1";
+            this.tbIP1.Size = new System.Drawing.Size(33, 20);
+            this.tbIP1.TabIndex = 1;
+            // 
+            // tbIP2
+            // 
+            this.tbIP2.Location = new System.Drawing.Point(80, 22);
+            this.tbIP2.MaxLength = 3;
+            this.tbIP2.Name = "tbIP2";
+            this.tbIP2.Size = new System.Drawing.Size(33, 20);
+            this.tbIP2.TabIndex = 2;
+            // 
+            // tbIP3
+            // 
+            this.tbIP3.Location = new System.Drawing.Point(119, 22);
+            this.tbIP3.MaxLength = 3;
+            this.tbIP3.Name = "tbIP3";
+            this.tbIP3.Size = new System.Drawing.Size(33, 20);
+            this.tbIP3.TabIndex = 3;
+            // 
+            // tbIP4
+            // 
+            this.tbIP4.Location = new System.Drawing.Point(158, 22);
+            this.tbIP4.MaxLength = 3;
+            this.tbIP4.Name = "tbIP4";
+            this.tbIP4.Size = new System.Drawing.Size(33, 20);
+            this.tbIP4.TabIndex = 4;
+            // 
+            // tbCIDR
+            // 
+            this.tbCIDR.Location = new System.Drawing.Point(218, 22);
+            this.tbCIDR.MaxLength = 2;
+            this.tbCIDR.Name = "tbCIDR";
+            this.tbCIDR.Size = new System.Drawing.Size(33, 20);
+            this.tbCIDR.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(197, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "/";
+            // 
+            // btDetailIP
+            // 
+            this.btDetailIP.Location = new System.Drawing.Point(278, 13);
+            this.btDetailIP.Name = "btDetailIP";
+            this.btDetailIP.Size = new System.Drawing.Size(91, 36);
+            this.btDetailIP.TabIndex = 7;
+            this.btDetailIP.Text = "Détails IP";
+            this.btDetailIP.UseVisualStyleBackColor = true;
+            this.btDetailIP.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbReseau
+            // 
+            this.lbReseau.AutoSize = true;
+            this.lbReseau.Location = new System.Drawing.Point(12, 105);
+            this.lbReseau.Name = "lbReseau";
+            this.lbReseau.Size = new System.Drawing.Size(50, 13);
+            this.lbReseau.TabIndex = 8;
+            this.lbReseau.Text = "@réseau";
+            // 
+            // lbDiffusion
+            // 
+            this.lbDiffusion.AutoSize = true;
+            this.lbDiffusion.Location = new System.Drawing.Point(12, 128);
+            this.lbDiffusion.Name = "lbDiffusion";
+            this.lbDiffusion.Size = new System.Drawing.Size(57, 13);
+            this.lbDiffusion.TabIndex = 9;
+            this.lbDiffusion.Text = "@diffusion";
+            // 
+            // lbPlage
+            // 
+            this.lbPlage.AutoSize = true;
+            this.lbPlage.Location = new System.Drawing.Point(12, 152);
+            this.lbPlage.Name = "lbPlage";
+            this.lbPlage.Size = new System.Drawing.Size(44, 13);
+            this.lbPlage.TabIndex = 10;
+            this.lbPlage.Text = "@plage";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "IP :";
+            // 
+            // lbMasque
+            // 
+            this.lbMasque.AutoSize = true;
+            this.lbMasque.Location = new System.Drawing.Point(12, 81);
+            this.lbMasque.Name = "lbMasque";
+            this.lbMasque.Size = new System.Drawing.Size(44, 13);
+            this.lbMasque.TabIndex = 12;
+            this.lbMasque.Text = "masque";
+            // 
+            // lbHote
+            // 
+            this.lbHote.AutoSize = true;
+            this.lbHote.Location = new System.Drawing.Point(12, 174);
+            this.lbHote.Name = "lbHote";
+            this.lbHote.Size = new System.Drawing.Size(28, 13);
+            this.lbHote.TabIndex = 13;
+            this.lbHote.Text = "hote";
+            // 
+            // lbErreur
+            // 
+            this.lbErreur.AutoSize = true;
+            this.lbErreur.Location = new System.Drawing.Point(35, 53);
+            this.lbErreur.Name = "lbErreur";
+            this.lbErreur.Size = new System.Drawing.Size(0, 13);
+            this.lbErreur.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(215, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "CIDR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 200);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbErreur);
+            this.Controls.Add(this.lbHote);
+            this.Controls.Add(this.lbMasque);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbPlage);
+            this.Controls.Add(this.lbDiffusion);
+            this.Controls.Add(this.lbReseau);
+            this.Controls.Add(this.btDetailIP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbCIDR);
+            this.Controls.Add(this.tbIP4);
+            this.Controls.Add(this.tbIP3);
+            this.Controls.Add(this.tbIP2);
+            this.Controls.Add(this.tbIP1);
+            this.Name = "Form1";
+            this.Text = "IP analyser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbIP1;
+        private System.Windows.Forms.TextBox tbIP2;
+        private System.Windows.Forms.TextBox tbIP3;
+        private System.Windows.Forms.TextBox tbIP4;
+        private System.Windows.Forms.TextBox tbCIDR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btDetailIP;
+        private System.Windows.Forms.Label lbReseau;
+        private System.Windows.Forms.Label lbDiffusion;
+        private System.Windows.Forms.Label lbPlage;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbMasque;
+        private System.Windows.Forms.Label lbHote;
+        private System.Windows.Forms.Label lbErreur;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace ProjetIP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //Procédure qui affiche les propriétés de l'ip saisie
+        public void afficheParam(ref int cidr)
+        {
+            if (cidr > 24)//on effectue une condition imbriqué car l'affichage des caractéristique de l'ip changeras en fonction du CIDR
+            {
+                lbMasque.Text = "Masque : 255.255.255." + masqueReseau(cidr);
+                lbReseau.Text = "Adresse Reseau : " + tbIP1.Text + "." + tbIP2.Text + "." + tbIP3.Text + "." + ipReseau(Convert.ToInt16(tbIP4.Text), cidr);
+                lbDiffusion.Text = "Adresse de diffusion : " + tbIP1.Text + "." + tbIP2.Text + "." + tbIP3.Text + "." + ipDiffusion(Convert.ToInt16(tbIP4.Text), cidr);
+                lbPlage.Text = "Plage d'adresse : " +  tbIP1.Text + "." + tbIP2.Text + "." + tbIP3.Text + "." + (Convert.ToInt16(ipReseau(Convert.ToInt16(tbIP4.Text), cidr))+1).ToString()//continue a la ligne suivante
+                + " --> " + tbIP1.Text + "." + tbIP2.Text + "." + tbIP3.Text + "." + (Convert.ToInt16(ipDiffusion(Convert.ToInt16(tbIP4.Text), cidr)) - 1).ToString();
+                lbHote.Text = "Nombre d'hotes : " + Convert.ToString(Math.Pow(2, (32 - cidr)) - 2);
+            }
+            else
+            {
+                if (cidr > 16)
+                {
+                    lbMasque.Text = "Masque : 255.255." + masqueReseau(cidr) + ".0";
+                    lbReseau.Text = "Adresse Reseau : " + tbIP1.Text + "." + tbIP2.Text + "." + ipReseau(Convert.ToInt16(tbIP3.Text), cidr) + ".0";
+                    lbDiffusion.Text = "Adresse de diffusion : " + tbIP1.Text + "." + tbIP2.Text + "." + ipDiffusion(Convert.ToInt16(tbIP3.Text), cidr) + ".255";
+                    lbPlage.Text = "Plage d'adresse : " + tbIP1.Text + "." + tbIP2.Text + "." + ipReseau(Convert.ToInt16(tbIP3.Text), cidr) + ".1" //continue sur la ligne suivante
+                    + " --> " + tbIP1.Text + "." + tbIP2.Text + "." + ipDiffusion(Convert.ToInt16(tbIP3.Text), cidr) + ".254";
+                    lbHote.Text = "Nombre d'hotes : " + Convert.ToString(Math.Pow(2, (32 - cidr)) - 2);
+                }
+                else
+                {
+                    if (cidr > 8)
+                    {
+                        lbMasque.Text = "Masque : 255." + masqueReseau(cidr) + ".0.0";
+                        lbReseau.Text = "Adresse Reseau : " + tbIP1.Text + "." + ipReseau(Convert.ToInt16(tbIP2.Text), cidr) + ".0.0";
+                        lbDiffusion.Text = "Adresse de diffusion : " + tbIP1.Text + "." + ipDiffusion(Convert.ToInt16(tbIP2.Text), cidr) + ".255.255";
+                        lbPlage.Text = "Plage d'adresse : " + tbIP1.Text + "." + ipReseau(Convert.ToInt16(tbIP2.Text), cidr) + ".0.1" + " --> " + //continue sur la ligne suivante
+                        tbIP1.Text + "." + ipDiffusion(Convert.ToInt16(tbIP2.Text), cidr) + ".255.254";
+                        lbHote.Text = "Nombre d'hotes : " + Convert.ToString(Math.Pow(2, (32 - cidr)) - 2);
+                    }
+                    else
+                    {
+                        lbMasque.Text = "Masque : " + masqueReseau(cidr) + ".0.0.0";
+                        lbReseau.Text = "Adresse Reseau : " + ipReseau(Convert.ToInt16(tbIP1.Text), cidr) + ".0.0.0";
+                        lbDiffusion.Text = "Adresse de diffusion : " + ipDiffusion(Convert.ToInt16(tbIP1.Text), cidr) + ".255.255.255";
+                        lbPlage.Text = "Plage d'adresse : " + ipReseau(Convert.ToInt16(tbIP1.Text), cidr) + ".0.0.1" + " --> " + ipDiffusion(Convert.ToInt16(tbIP1.Text), cidr) + ".255.255.254";
+                        lbHote.Text = "Nombre d'hotes : " + Convert.ToString(Math.Pow(2, (32 - cidr)) - 2);
+                    }
+
+                }
+            }
+        }
+        //Cette Fonction permet de récupéré la partie réseau de l'ip saisie
+        public string ipReseau(int ip, int cidr)
+        {
+            //On convertie l'ip saisie en base 2
+            string ipBinaire = Convert.ToString(ip, 2);
+            //Comme on travail sur un octet il faut vérifié que l'ip en base 2 est bien contenu sur une chaine de taille 8
+            //la conversion en base 2 ne complete pas avec des 0 / exemple 54 = 110110
+            if (ipBinaire.Length != 8)
+            {
+                string complement = "";
+                for (int j = 0; j < 8 - ipBinaire.Length; j++)
+                {
+                    complement = complement + "0";
+                }
+                ipBinaire = complement + ipBinaire;
+            }
+            //apres cette condition 54 seras égal a 00110110
+
+
+            string reste = "";
+            //cette boucle permet de compléter l'ip réseau avec des 0 de sorte a ce que la chaine est une taille de 8 caractères
+            for (int j = 0; j < 8 - (cidr%8); j++)
+            {
+                reste = reste + "0";
+            }
+            //on utilise la méthode substring pour ne prendre que les bits réseau et la boucle précédente pour compléter avec des 0
+            //exemple 192.168.1.10 / 12 on utilise 4 bit pour lé réseau donc 1010 | puis on complte avec des 0
+            string ipReseau = ipBinaire.Substring(0, cidr%8) + reste;//suivant l'exemple ipRéseau 1010|0000
+            ipReseau = Convert.ToInt16(ipReseau, 2).ToString();//on reconvertit l'ip base 2 en base 10
+
+            return ipReseau;//on retourne cette ip
+        }
+
+
+        public string ipDiffusion(int ip, int cidr)//la fonction diffusion est similaire a réseau on compléte juste avec des 1 au lieu de 0
+        {
+            string ipBinaire = Convert.ToString(ip, 2);
+            if (ipBinaire.Length != 8)
+            {
+                string complement = "";
+                for (int j = 0; j < 8 - ipBinaire.Length; j++)
+                {
+                    complement = complement + "0";
+                }
+                ipBinaire = complement + ipBinaire;
+            }
+            string reste = "";
+            for (int j = 0; j <8 - (cidr%8); j++)
+            {
+                reste = reste + "1";
+            }
+            //on complete avec des 1
+            string ipReseau = ipBinaire.Substring(0, cidr%8) + reste;
+            ipReseau = Convert.ToInt16(ipReseau, 2).ToString();
+
+            return ipReseau;
+        }
+
+        //la fonction masque permet d'obtenir l'adresse du masque réseau
+        public string masqueReseau(int cidr)
+        {
+            string ip = "";
+            //on convertit le CIDR compris entre 1 et 8 en uen chaine de caractère "1"
+            for (int i = 0; i < cidr%8; i++)
+            {
+                ip = ip + "1";
+            }
+
+            string reste = "";
+            //comme pour les méthodes diffusion et réseau on compléte avec des "0" pour obtenir un octet
+            for (int i = 0; i < 8 - (cidr%8); i++)
+            {
+                reste = reste + "0";
+            }
+            //exemple un CIDR de 28 seras ramené a 4 ( comme > 24 | 28-24 =4)
+            //il y aura donc un octet de la forme 11110000
+
+            //on convertit l'octet en base 10 et on retourne sa valeur
+            return Convert.ToInt16(ip + reste, 2).ToString();
+        }
+
+        public bool verifSaisie(string s)
+        {
+            int i;
+            //la méthode TryParse permet de tester le type de variable saisie dans la textbox si c'est un entier cela retourne la valeur True
+            bool saisie = Int32.TryParse(s, out i);
+            return saisie;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (tbIP2.Text == "" || tbIP1.Text == "" || tbIP3.Text == "" || tbIP3.Text == "" || tbCIDR.Text == "")
+            {
+                
+                lbErreur.Text = "Erreur : Il est nécessaire que tout les champs soient remplis";
+            }
+            else
+            {
+                if (verifSaisie(tbCIDR.Text) == false)
+                {
+                    lbErreur.Text = "Erreur : Le CIDR doit etre un chiffre";
+                }
+                else
+                {
+                    if (verifSaisie(tbIP2.Text) == false || verifSaisie(tbIP1.Text) == false || verifSaisie(tbIP3.Text) == false || verifSaisie(tbIP4.Text) == false)
+                    {
+                        lbErreur.Text = "Erreur : L'ip doit etre un chiffre";
+                    }
+                    else
+                    {
+                        if ((Convert.ToInt16(tbIP2.Text) > 255 || Convert.ToInt16(tbIP2.Text) < 0) || (Convert.ToInt16(tbIP1.Text) > 255 || Convert.ToInt16(tbIP1.Text) < 0) || (Convert.ToInt16(tbIP3.Text) > 255 || Convert.ToInt16(tbIP3.Text) < 0) || (Convert.ToInt16(tbIP4.Text) > 255 || Convert.ToInt16(tbIP4.Text) < 0))
+                        {
+                            lbErreur.Text = "Erreur : Une IP est codé de 1 à 255";
+                        }
+                        else
+                        {
+                            if (Convert.ToInt16(tbCIDR.Text) > 31 || Convert.ToInt16(tbCIDR.Text) < 1)
+                            {
+                                lbErreur.Text = "Erreur :  Le CIDR est compris de 1 a 31";
+                            }
+                            else
+                            {
+                                int cidr = Convert.ToInt16(tbCIDR.Text);
+                                afficheParam(ref cidr);
+                                lbErreur.Text = "";
+
+                            }
+                        }
+
+                    }
+
+                }
+            }
+
+        }
+    }
+
+}
+
